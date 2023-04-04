60a385a59934a5fc781c8d607fb719120f3df0fa
Oni <ylies99@yahoo.com>
2023-04-04 19:57:14 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports= {
+    token: "MTA4MzI1ODA1OTgxMzU2ODU3Mg.G-_IvY.ChiL4r_9-oIUhUEDIIWtuYG4M7Ek2FwyGnX_7I",
+    prefix: '.',
+    owner: [
+        //Sown
+        "648236998657835047", 
+        //AYANO 
+        "1017475706613157899",
+        //Karma 
+        "1055889359334813847",
+		//Atoya 
+        "1017475706613157899"
+    ],
+    footer: "Cloud's Manager",
+    support: "https://discord.gg/MG7gZgkftR",
+    channel: '1089593266351124543',
+    guild: '1089593266351124540',
+    client: '1089604608080494594',
+    ptero: "ptla_ceF96Yuwafz5oZkasjBcZhBq7RsUWRlsatcGzZnTVen",
+    color: '#ffffff'
+}
